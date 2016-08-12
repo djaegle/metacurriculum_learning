5f4d0dea84a45d92b99e5b6f9454bc3a242b616e
Drew Jaegle <drewjaegle@gmail.com>
2016-08-12 12:56:45 -0400

    Forward and backward pass check out, for both random and correct settings.

--- tests/ElemwiseCrossEntropyCheck.lua
+++ tests/ElemwiseCrossEntropyCheck.lua
@@ -6,63 +6,45 @@ require '../ElemwiseClassNLL.lua'
 require '../ElemwiseCrossEntropy.lua'
 debugger = require('fb.debugger')
 
-module = nn.ElemwiseCrossEntropy()
---module = nn.ElemwiseClassNLL()
+local n_classes = 10
+local n_ims = 5
 
--- input_img = torch.rand(5,10) -- 5 ims, 10 classes
-target_class = 3
+-- Correct setting:
+local target_class = 3
+local target = torch.Tensor(n_ims):fill(target_class)
+local input_img = torch.Tensor(n_ims,n_classes):fill(-100)
+input_img[{{},{target_class}}] = 100
 
--- Correct setting
--- input_img = torch.Tensor(5,10):fill(-100)
--- input_img[{{},{target_class}}] = 100
 
--- Random setting
-input_img = torch.rand(5,10)
+-- Random guess setting
+-- local target = torch.Tensor(n_ims):random(1,n_classes)
+-- local input_img = torch.rand(n_ims,n_classes)
 
-input = {input_img,target}
 
-function feval(x)
-  local target = torch.Tensor(5):fill(target_class)
-  -- debugger.enter()
+local input = {input_img,target}
+
+function forward_est(x,target)
+  local module = nn.ElemwiseCrossEntropy()
+
   input = {x,target}
-  module:forward(input)
-  module:backward(input, torch.ones(input[2]:size()))
-  return module.output:sum(), module.gradInput:reshape(input[1]:numel())
+  local err = module:forward(input)
+  -- Put in a setting where this is mimicking acting as the loss:
+  -- The dL/doutput is just 1/n_ims - average the imwise losses
+  local t = module:backward(input, torch.Tensor(input[2]:size()):fill(1/n_ims))
+  return torch.mean(err), t
 end
 
-function forward_est(x)
-  local target = torch.Tensor(5):fill(target_class)
-  -- debugger.enter()
-  input = {x,target}
-  module:forward(input)
-  module:backward(input, torch.ones(input[2]:size()))
-  return module.output
-end
-
-function true_forward_est(x)
-  local target = torch.Tensor(5):fill(target_class)
-  local criterion = nn.ClassNLLCriterion()
+function true_forward_est(x,target)
+  local criterion = nn.CrossEntropyCriterion()
   local err = criterion:forward(x,target)
-  return err
+  local t = criterion:backward(x,target)
+  return err, t
 end
-
-diff,dc,dc_est = optim.checkgrad(feval,input_img)
--- print('Diff must be close to 1e-8: diff = ' .. diff)
--- print(dc)
--- print(dc_est)
-
-
--- Try it manually:
-eps = 1e-5
-input_img2 = torch.Tensor(input_img:size()):copy(input_img)
-input_img2[{{1},{1}}] = input_img2[{{1},{1}}] + eps
-
-input_img3 = torch.Tensor(input_img:size()):copy(input_img)
-input_img3[{{1},{1}}] = input_img3[{{1},{1}}] - eps
-dinput = (forward_est(input_img3)-forward_est(input_img2))/(2*eps)
-print(dinput)
 
 -- Test by comparing results with the built-in ClassNLLCriterion
-test_result = forward_est(input_img)
-true_result = true_forward_est(input_img)
-print('Diff between avg of ElemwiseClassNLL and built-in should be small: diff = ' .. torch.mean(test_result)-true_result)
+test_result, test_deriv = forward_est(input_img,target)
+true_result, true_deriv = true_forward_est(input_img,target)
+print('Forward: Diff between avg of ElemwiseClassNLL and built-in should be small: diff = '
+  .. test_result-true_result)
+print('Backward: Diff between avg of ElemwiseClassNLL and built-in should be small: diff: ')
+print(test_deriv-true_deriv)
